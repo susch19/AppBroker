0c7cfbd89acf79cbfc12e14a005e678513778d3f
susch19 <suschi@ist-einmalig.de>
2021-08-28 21:31:24 +0200

    Fixed osram switch not togleable
    * Because someone, i don't want to say out loud who, has forgotten to implement the update from app logic

--- AppBokerASP/Devices/Zigbee/OsramPlug.cs
+++ AppBokerASP/Devices/Zigbee/OsramPlug.cs
@@ -1,4 +1,8 @@
-﻿using SocketIOClient;
+﻿using Newtonsoft.Json.Linq;
+
+using PainlessMesh;
+
+using SocketIOClient;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +17,23 @@ namespace AppBokerASP.Devices.Zigbee
         public OsramPlug(long nodeId, string baseUpdateUrl, SocketIO socket) : base(nodeId, baseUpdateUrl, typeof(OsramPlug), socket)
         {
             ShowInApp = true;
+        }
+
+        public override void UpdateFromApp(Command command, List<JToken> parameters)
+        {
+            switch (command)
+            {
+                case Command.On:
+                    State = true;
+                    _ = UpdateZigbeeDeviceRequest(nameof(State).ToLower(), State.ToString().ToLower());
+                    break;
+                case Command.Off:
+                    State = false;
+                    _ = UpdateZigbeeDeviceRequest(nameof(State).ToLower(), State.ToString().ToLower());
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
