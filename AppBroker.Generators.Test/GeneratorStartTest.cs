0bee2ebd43821855596647de3b6ce63e187798af
susch19 <suschi@ist-einmalig.de>
2021-10-16 11:59:02 +0200

    Implemented app broker property changed generator
    * because we have a very special use case so a very special generator is required

---
+++ AppBroker.Generators.Test/GeneratorStartTest.cs
@@ -0,0 +1,82 @@
+using NUnit.Framework;
+
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+using System;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using System.IO;
+
+namespace AppBroker.Generators.Test
+{
+    [Explicit]
+    public class Tests
+    {
+        public static Dictionary<string, string> GetGeneratedOutput<TGenerator>(IDictionary<string, string> sources, bool failOnInvalidSource = false, bool failOnDiagnostigs = true)
+           where TGenerator : ISourceGenerator, new()
+           => GetGeneratedOutput(sources, () => new TGenerator(), failOnInvalidSource, failOnDiagnostigs);
+
+        public static CSharpCompilation Compile(IDictionary<string, string> sources)
+                    => CSharpCompilation.Create(
+                        "test",
+                        sources.Select(x => CSharpSyntaxTree.ParseText(x.Value, path: x.Key)),
+                        new[]
+                        {
+                            MetadataReference.CreateFromFile(typeof(Binder).GetTypeInfo().Assembly.Location),
+                            MetadataReference.CreateFromFile(typeof(INotifyPropertyChanged).GetTypeInfo().Assembly.Location),
+                        },
+                        new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+
+        public static Dictionary<string, string> GetGeneratedOutput(IDictionary<string, string> sources, Func<ISourceGenerator> makeGenerator, bool failOnInvalidSource = false, bool failOnDiagnostigs = true)
+        {
+            var compilation = Compile(sources);
+
+            if (failOnInvalidSource)
+            {
+                FailIfError(compilation.GetDiagnostics());
+            }
+
+            var generator = makeGenerator();
+
+            var driver = CSharpGeneratorDriver.Create(generator);
+            _ = driver.RunGeneratorsAndUpdateCompilation(compilation, out var outputCompilation, out var generateDiagnostics);
+            var output = outputCompilation.SyntaxTrees.ToDictionary(tree => tree.FilePath, tree => tree.ToString());
+
+            if (failOnDiagnostigs)
+            {
+                FailIfError(generateDiagnostics);
+            }
+
+            return output;
+        }
+
+        private static void FailIfError(IEnumerable<Diagnostic> diag)
+        {
+            var errors = diag.Where(d => d.Severity == DiagnosticSeverity.Error);
+            var msg = "Failed: " + errors.FirstOrDefault()?.GetMessage();
+            Assert.That(errors, Is.Empty, msg);
+        }
+
+        //private AdhocWorkspace Workspace;
+
+        [SetUp]
+        public void Setup()
+        {
+            //Workspace = new();
+        }
+
+        [Test, Explicit]
+        public void DebugGenerator()
+        {
+            var input = File.ReadAllText(@"C:\Users\susch\source\repos\AppBroker\TestGenerator\PropChanged.cs");
+            var sources = new Dictionary<string, string>()
+            {
+                { "test.cs", input }
+            };
+            var outputs = GetGeneratedOutput<NotifyPropertyChangedGenerator>(sources, failOnInvalidSource: false, failOnDiagnostigs: false);
+            ;
+        }
+    }
+}
---
+++ TestGenerator/PropChanged.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGenerator
+{
+    [AppBroker.ClassPropertyChangedAppbroker(true, false)]
+    public partial class PropChanged
+    {
+        private int myProperty;
+        
+        [AppBroker.IgnoreChangedAppbroker]
+        private int myProperty2;
+
+        [AppBroker.PropertyChangedAppbroker(PropertyName = "Different")]
+        private int myProperty3;
+
+        protected virtual void OnPropertyChanging<T>(ref T field, T value, string propertyName)
+        {
+
+        }
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+
+        }
+    }
+}
