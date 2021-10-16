e98306b1c9e9ce76f43fd70b519609c0ca3413c2
susch19 <suschi@ist-einmalig.de>
2021-10-16 21:42:31 +0200

    Implemented possibility to copy attribute of other property for generated props
    * so json ignore or json name property is possible
    
    Co-Authored-By: blubbi <henk.daniel98@gmail.com>

--- TestGenerator/PropChanged.cs
+++ TestGenerator/PropChanged.cs
@@ -1,11 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace TestGenerator
 {
+    public class TestAttributeWithParameterAttribute : Attribute
+    {
+        public TestAttributeWithParameterAttribute(string name)
+        {
+            PropName = name;
+        }
+
+        public string PropName { get;  }
+    }
+
     [AppBroker.ClassPropertyChangedAppbroker(true, false)]
     public partial class PropChanged
     {
@@ -14,8 +26,11 @@ namespace TestGenerator
         [AppBroker.IgnoreChangedAppbroker]
         private int myProperty2;
 
-        [AppBroker.PropertyChangedAppbroker(PropertyName = "Different")]
+        [AppBroker.PropertyChangedAppbroker(PropertyName = "Different"), AppBroker.CopyPropertyAttributesFromAttribute(nameof(property))]
         private int myProperty3;
+
+        [JsonIgnore(), Description("For"), JsonPropertyName("Test")]
+        private bool property { get; }
 
         protected virtual void OnPropertyChanging<T>(ref T field, T value, string propertyName)
         {
