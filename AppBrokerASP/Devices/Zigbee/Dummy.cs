e3fddfcad4ee09848d1b499458a3440cb09fe8f9
susch19 <suschi@ist-einmalig.de>
2022-01-01 17:54:53 +0100

    Done stuff

---
+++ AppBrokerASP/Devices/Zigbee/Dummy.cs
@@ -0,0 +1,11 @@
+﻿using SocketIOClient;
+
+namespace AppBrokerASP.Devices.Zigbee;
+
+[DeviceName("SPZB0001")]
+public class Dummy : UpdateableZigbeeDevice
+{
+    public Dummy(long nodeId, SocketIO socket) : base(nodeId, socket)
+    {
+    }
+}
