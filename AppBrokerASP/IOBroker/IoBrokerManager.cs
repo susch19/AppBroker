2c596762d0556be85f0010419d5d62ed82843e0f
Daniel Henk <henk.daniel98@gmail.com>
2022-07-30 10:18:15 +0200

    Move iobroker into own class

--- AppBrokerASP/IOBroker/IoBrokerManager.cs
+++ AppBrokerASP/IOBroker/IoBrokerManager.cs
@@ -1,121 +1,42 @@
-using System.Reflection;
-using System.Text;
-
+﻿using AppBrokerASP.Configuration;
+using AppBrokerASP.Devices.Zigbee;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using AppBrokerASP.Extension;
+using SocketIOClient;
 using AppBrokerASP.Database;
-using AppBrokerASP.Devices;
-using AppBrokerASP.IOBroker;
-
-using PainlessMesh;
-using AppBrokerASP.Configuration;
-using AppBroker.Core;
 using AppBroker.Core.Devices;
 
-namespace AppBrokerASP;
+namespace AppBrokerASP.IOBroker;
 
-public class DeviceManager : IDisposable, IDeviceManager
+public class IoBrokerManager
 {
-    public ZigbeeConfig Config { get; }
-    public ConcurrentDictionary<long, Device> Devices { get; } = new();
-    public IReadOnlyCollection<Type> DeviceTypes => types;
-
     public event EventHandler<(long id, Device device)>? NewDeviceAdded;
 
-    private bool disposed;
-    private readonly NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
-    private readonly List<Type> types;
-    private readonly Dictionary<string, Type> alternativeNamesForTypes = new(StringComparer.OrdinalIgnoreCase);
-    private static readonly HttpClient http = new();
-    private readonly IoBrokerManager? ioBrokerManager;
+    private SocketIO? client;
+    private readonly NLog.Logger logger;
+    private readonly ZigbeeConfig Config;
+    private readonly HttpClient http;
+    private readonly ConcurrentDictionary<long, Device> Devices;
+    private readonly Dictionary<string, Type> alternativeNamesForTypes;
 
-    public DeviceManager()
+    public IoBrokerManager(NLog.Logger logger, HttpClient http, ConcurrentDictionary<long, Device> devices, Dictionary<string, Type> alternativeNamesForTypes, ZigbeeConfig config)
     {
-        Config = InstanceContainer.Instance.ConfigManager.ZigbeeConfig;
-
-        types = Assembly
-            .GetExecutingAssembly()
-            .GetTypes()
-            .Where(x => typeof(Device).IsAssignableFrom(x) && x != typeof(Device))
-            .ToList();
-
-        foreach (var type in types)
-        {
-            var names = GetAllNamesFor(type);
-            foreach (var name in names)
-            {
-                alternativeNamesForTypes[name] = type;
-            }
-        }
-
-        InstanceContainer.Instance.MeshManager.NewConnectionEstablished += Node_NewConnectionEstablished;
-        InstanceContainer.Instance.MeshManager.ConnectionLost += MeshManager_ConnectionLost;
-        InstanceContainer.Instance.MeshManager.ConnectionReastablished += MeshManager_ConnectionReastablished;
-
-        if (Config.Enabled is null or true)
-        {
-            ioBrokerManager = new IoBrokerManager(logger, http, Devices, alternativeNamesForTypes, Config);
-            ioBrokerManager.NewDeviceAdded += NewDeviceAdded;
-            _ = ioBrokerManager.ConnectToIOBroker();
-        }
-    }
-    private void AddNewDeviceToDic(Device device)
-    {
-        if (Devices.TryAdd(device.Id, device))
-            NewDeviceAdded?.Invoke(this, (device.Id, device));
+        this.logger = logger;
+        this.http = http;
+        this.alternativeNamesForTypes = alternativeNamesForTypes;
+        Devices = devices;
+        Config = config;
     }
 
-    private static List<string> GetAllNamesFor(Type y)
+    // TODO: is copy pasted and duplicated in devicemanager
+    private void AddNewDeviceToDic(long id, Device device)
     {
-        var names = new List<string>();
-        var attribute = y.GetCustomAttribute<DeviceNameAttribute>();
-
-        if (attribute is not null)
-        {
-            names.Add(attribute.PreferredName);
-            names.AddRange(attribute.AlternativeNames);
-        }
-        names.Add(y.Name);
-        return names;
+        if (Devices.TryAdd(id, device))
+            NewDeviceAdded?.Invoke(this, (id, device));
     }
 
-    private void MeshManager_ConnectionLost(object? sender, uint e)
-    {
-        if (Devices.TryGetValue(e, out var device))
-        {
-            device.StopDevice();
-        }
-    }
-    private void MeshManager_ConnectionReastablished(object? sender, (uint id, ByteLengthList parameter) e)
-    {
-        if (Devices.TryGetValue(e.id, out var device))
-        {
-            device.Reconnect(e.parameter);
-        }
-    }
-
-    private void Node_NewConnectionEstablished(object? sender, (Sub c, ByteLengthList l) e)
-    {
-        if (Devices.TryGetValue(e.c.NodeId, out var device))
-        {
-            device.Reconnect(e.l);
-        }
-        else
-        {
-            var deviceName = Encoding.UTF8.GetString(e.l[1]);
-            var newDevice = CreateDeviceFromName(deviceName, e.c.NodeId, e.l);
-
-            if (newDevice is null)
-                return;
-
-            logger.Debug($"New Device: {newDevice.TypeName}, {newDevice.Id}");
-            AddNewDeviceToDic( newDevice);
-            //_ = Devices.TryAdd(e.c.NodeId, newDevice);
-
-            if (!DbProvider.AddDeviceToDb(newDevice))
-                _ = DbProvider.MergeDeviceWithDbData(newDevice);
-
-        }
-    }
-
+    // TODO: is copy pasted and duplicated in devicemanager
     private Device? CreateDeviceFromName(string deviceName, params object[] ctorArgs)
     {
         logger.Trace($"Trying to get device with {deviceName} name");
@@ -135,6 +56,69 @@ public class DeviceManager : IDisposable, IDeviceManager
         }
 
         return newDevice;
+    }
+
+    public async Task ConnectToIOBroker()
+    {
+        client = new SocketIO(new Uri(Config.SocketIOUrl), new SocketIOOptions()
+        {
+            EIO = Config.NewSocketIoversion ? 4 : 3
+        });
+
+        client.OnAny(async (eventName, response) =>
+        {
+            if (IoBrokerZigbee.TryParse(eventName, response.ToString(), out var zo) && zo is not null)
+            {
+                if (Devices.TryGetValue(zo.Id, out var dev) && dev is ZigbeeDevice zigbeeDev)
+                {
+                    try
+                    {
+                        zigbeeDev.SetPropFromIoBroker(zo, true);
+                    }
+                    catch (Exception e)
+                    {
+                        logger.Error(e);
+                    }
+                }
+                else
+                {
+                    try
+                    {
+
+                        await GetZigbeeDevices(client);
+                    }
+                    catch (Exception ex)
+                    {
+                        logger.Error(ex);
+                    }
+                }
+            }
+        });
+
+        client.OnError += async (sender, args) =>
+        {
+            logger.Error($"AfterException, trying reconnect: {args}");
+            await client.DisconnectAsync();
+            await client.ConnectAsync();
+        };
+
+        client.OnConnected += async (s, e) =>
+        {
+            logger.Debug("Connected Zigbee Client");
+            await client.EmitAsync("subscribe", "zigbee.*");
+            await client.EmitAsync("subscribeObjects", '*');
+            try
+            {
+
+                await GetZigbeeDevices(client);
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex);
+            }
+        };
+
+        await client.ConnectAsync();
     }
 
     public async Task GetZigbeeDevices(SocketIO socket)
@@ -210,7 +194,6 @@ public class DeviceManager : IDisposable, IDeviceManager
         var content = await http.GetStringAsync(idRequest);
 
         var getDeviceResponses = JsonConvert.DeserializeObject<IoBrokerGetDeviceResponse[]>(content)!;
-
         var deviceStates = new List<IoBrokerStateResponse>();
 
         foreach (var stateRequest in requests)
@@ -274,32 +257,4 @@ public class DeviceManager : IDisposable, IDeviceManager
         }
     }
 
-    protected virtual void Dispose(bool disposing)
-    {
-        if (!disposed)
-        {
-            if (disposing)
-            {
-                foreach (var device in Devices.Values.OfType<IDisposable>())
-                {
-                    device?.Dispose();
-                }
-                Devices.Clear();
-
-                if (ioBrokerManager is not null)
-                {
-                    ioBrokerManager.NewDeviceAdded -= NewDeviceAdded;
-                }
-            }
-
-            disposed = true;
-        }
-    }
-
-    public void Dispose()
-    {
-        // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
-        Dispose(disposing: true);
-        GC.SuppressFinalize(this);
-    }
 }
