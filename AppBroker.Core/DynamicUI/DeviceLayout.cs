11c14ac7fef0b8a9737052fb8d91bb610cc57878
susch19 <suschi@ist-einmalig.de>
2022-09-14 18:54:54 +0200

    Added support for developer mode only devices
    * so this way you can have a device to control things on a server, without having it visible all the time

--- AppBroker.Core/DynamicUI/DeviceLayout.cs
+++ AppBroker.Core/DynamicUI/DeviceLayout.cs
@@ -3,4 +3,12 @@
 namespace AppBroker.Core.DynamicUI;
 
 
-public record DeviceLayout(string UniqueName, string? TypeName, string[]? TypeNames, long[]? Ids, DashboardDeviceLayout? DashboardDeviceLayout, DetailDeviceLayout? DetailDeviceLayout, int Version = 1);
+public record DeviceLayout(
+    string UniqueName, 
+    string? TypeName,
+    string[]? TypeNames,
+    long[]? Ids, 
+    DashboardDeviceLayout? DashboardDeviceLayout, 
+    DetailDeviceLayout? DetailDeviceLayout, 
+    int Version = 1, 
+    bool ShowOnlyInDeveloperMode = false);
