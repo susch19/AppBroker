0f4dd808dea3c59616c451fb57536d423ed0c1a9
Daniel Henk <henk.daniel98@gmail.com>
2022-11-12 22:50:25 +0100

    Update BrokerDbContext.cs

--- AppBroker.Core/Database/BrokerDbContext.cs
+++ AppBroker.Core/Database/BrokerDbContext.cs
@@ -36,11 +36,11 @@ public class BrokerDbContext : BaseDbContext
 /*
  * Migration Steps:
  * Exectue SQL in db before executin application: 
-CREATE TABLE "__EFMigrationsHistory" (
+CREATE TABLE IF NOT EXISTS "__EFMigrationsHistory" (
     "MigrationId" TEXT NOT NULL CONSTRAINT "PK___EFMigrationsHistory" PRIMARY KEY,
     "ProductVersion" TEXT NOT NULL
 );
 
-Insert into "__EFMigrationsHistory" VALUES("20220923182559_Initial", "6.0.7")
+Insert into "__EFMigrationsHistory" SELECT "20220923182559_Initial", "6.0.7" WHERE NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE MigrationId = "20220923182559_Initial");
  
  */
